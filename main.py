3a16c2543f662c528719048638293b3e8e92733a
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-09-18 15:56:25 +0100

    character.py file created

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame
+import constants
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+pygame.display.caption("Space Adventure")
 
 run = True
 while run:
