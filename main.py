ad207b0cff55a0f72f2c2a331090525a80067381
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-09-21 06:35:20 +0100

    window working

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from character import Character
 pygame.init()
 
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-pygame.display.caption("Space Adventure")
+pygame.display.set_caption("Space Adventure")
 
 #player creation
 player = Character(100, 100)
@@ -13,7 +13,7 @@ player = Character(100, 100)
 run = True
 while run:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             run = False
 
 pygame.quit()
